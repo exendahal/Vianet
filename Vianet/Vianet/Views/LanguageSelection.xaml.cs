1c4546e1eeb01929447939e236eaca6aa22a7802
santosh dahal <exendahal@gmail.com>
2021-06-05 17:07:40 +0545

    welcome page complete

--- Vianet/Vianet/Views/LanguageSelection.xaml.cs
+++ Vianet/Vianet/Views/LanguageSelection.xaml.cs
@@ -12,9 +12,59 @@ namespace Vianet.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LanguageSelection : Rg.Plugins.Popup.Pages.PopupPage
     {
+
         public LanguageSelection()
         {
             InitializeComponent();
+            if (App.Current.Properties.ContainsKey("isEnglish"))
+            {
+                if (Application.Current.Properties["isEnglish"].ToString() == "1")
+                {
+                    englishFrame.BackgroundColor = (Color)Application.Current.Resources["MainColor"];
+                    englishText.TextColor = (Color)Application.Current.Resources["MainColor"];
+                    langColor.TextColor = Color.White;
+                    nepaliFrame.BackgroundColor = Color.FromHex("#F7F2F2");
+                    nepaliText.TextColor = Color.FromHex("#999999");
+                    nepLang.TextColor = (Color)Application.Current.Resources["MainColor"];
+                }
+                else
+                {
+                    nepaliFrame.BackgroundColor = (Color)Application.Current.Resources["MainColor"];
+                    nepaliText.TextColor = (Color)Application.Current.Resources["MainColor"];
+                    nepLang.TextColor = Color.White;
+                    englishFrame.BackgroundColor = Color.FromHex("#F7F2F2");
+                    englishText.TextColor = Color.FromHex("#999999");
+                    langColor.TextColor = (Color)Application.Current.Resources["MainColor"];
+                }
+            }
+            
+        }
+
+        private void TapGestureRecognizer_Tapped_english(object sender, EventArgs e)
+        {
+            englishFrame.BackgroundColor = (Color)Application.Current.Resources["MainColor"];
+            englishText.TextColor = (Color)Application.Current.Resources["MainColor"];
+            langColor.TextColor = Color.White;
+
+            nepaliFrame.BackgroundColor = Color.FromHex("#F7F2F2");
+            nepaliText.TextColor = Color.FromHex("#999999");
+            nepLang.TextColor = (Color)Application.Current.Resources["MainColor"];
+            Application.Current.Properties["isEnglish"] = "1";
+            MessagingCenter.Send(this, "Language", "English");
+        }
+
+        private void TapGestureRecognizer_Tapped_nepali(object sender, EventArgs e)
+        {
+            nepaliFrame.BackgroundColor = (Color)Application.Current.Resources["MainColor"];
+            nepaliText.TextColor = (Color)Application.Current.Resources["MainColor"];
+            nepLang.TextColor = Color.White;
+
+            englishFrame.BackgroundColor = Color.FromHex("#F7F2F2");
+            englishText.TextColor = Color.FromHex("#999999"); 
+            langColor.TextColor = (Color)Application.Current.Resources["MainColor"];
+            Application.Current.Properties["isEnglish"] = "0";
+            MessagingCenter.Send(this, "Language", "नेपाली");
+
         }
     }
 }
--- Vianet/Vianet/Views/Login.xaml.cs
+++ Vianet/Vianet/Views/Login.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,11 +17,31 @@ namespace Vianet.Views
         public Login()
         {
             InitializeComponent();
+            if (App.Current.Properties.ContainsKey("isEnglish"))
+            {
+                if (Application.Current.Properties["isEnglish"].ToString() == "1")
+                {
+                    DefaultLanguage.Text = "English";
+                }
+                else
+                {
+                    DefaultLanguage.Text = "नेपाली";
+                }
+             }
         }
 
         private async void TapGestureRecognizer_Tapped_language(object sender, EventArgs e)
         {
             await Navigation.PushPopupAsync(new LanguageSelection());
+            MessagingCenter.Subscribe<LanguageSelection, string>(this, "Language",
+               async(page, data) =>
+               {
+
+                   DefaultLanguage.Text = data;
+                   await PopupNavigation.Instance.PopAsync();
+                   MessagingCenter.Unsubscribe<LanguageSelection, string>(this, "Language");
+
+               });
         }
     }
 }
